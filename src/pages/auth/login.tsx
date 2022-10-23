52293acba9f2fd68056cc17df1f3fb7787cbbe1e
Barack Mukelenga <barackmukelenga100@gmail.com>
2022-10-23 18:19:20 +0200

    Adjust the auth screens height on small screens

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -30,10 +30,10 @@ const Login = () => {
   };
 
   return (
-    <div className='flex justify-center align-middle w-screen h-screen  '>
-      <div className='flex justify-center align-middle w-[100%] sm:w-[90%] md:w-[60%] h-full lg:w-[500px] xl:w-w-[400px] 2xl:w-[500px] overflow-y-auto'>
-        <div className='flex flex-col justify-center align-middle w-[90%] max-h-[90%] max-w-[90%] '>
-          <div className='flex justify-center align-middle w-full bg-white rounded-xl shadow-soft-3xl flex-col  py-16 px-6 my-20'>
+    <div className='flex fixed top-0 left-0 right-0 bottom-0 items-start w-full h-full justify-center overflow-y-auto'>
+      <div className='flex  justify-center w-[100%] sm:w-[90%] md:w-[60%] h-full lg:w-[500px] xl:w-w-[400px] 2xl:w-[500px] '>
+        <div className='flex flex-col items-start w-[90%] max-w-[90%] mt-20'>
+          <div className='flex w-full bg-white rounded-xl shadow-soft-3xl flex-col  py-16 px-6'>
             <div className='flex justify-center align-middle w-full h-20 bg-primary rounded-t-xl mb-5'>
               <div className='flex justify-center flex-col align-middle w-full text-center'>
                 <div className='flex justify-center align-middle w-full'>
